ea03cfd36c13f8a8a9c59114704bcb237a5027a9
zzs <zzspku@qq.com>
2017-06-09 15:04:30 +0800

    两个页面

--- src/ProductNav.js
+++ src/ProductNav.js
@@ -28,18 +28,6 @@ class ProductNav extends Component {
             <p className="subnav-title">VR实训空间</p>
           </a>
         </section>
-        <section className="news-subnav">
-          <a href="" className="subnav-item" id="subnav-item1">
-            <div className="mask"></div>
-            <img src="static/image/subnav_news_1_icon.png" alt="" className="subnav-icon"/>
-            <p className="subnav-title">微视酷新闻</p>
-          </a>
-          <a href="" className="subnav-item" id="subnav-item2">
-            <div className="mask"></div>
-            <img src="static/image/subnav_news_2_icon.png" alt="" className="subnav-icon"/>
-            <p className="subnav-title">行业资讯</p>
-          </a>
-        </section>
       </section>
     );
   }
