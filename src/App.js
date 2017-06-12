99be22a0cd43e8b44d6a3e7fcfad35f67d0d5fcb
zzs <zzspku@qq.com>
2017-06-12 20:10:36 +0800

    让陈佩检查

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import './home.css';
 import Home from './Home.js';
 
 class App extends Component {
--- src/Product.js
+++ src/Product.js
@@ -5,36 +5,39 @@ import HomeFooter from './HomeFooter.js';
 var $ = require('jquery');
 
 class Product extends Component {
-  render() {
-    return (
-      <section className="product">
-        <HomeHeader />
-        <section className="product-index">
-          <a href="" className="item" id="item1">
-            <h2 className="item-title">IES系统</h2>
-            <div className="item-icon"></div>
-            <p className="item-link">了解更多→</p>
-          </a>
-          <a href="" className="item" id="item2">
-            <h2 className="item-title">VR课堂</h2>
-            <div className="item-icon"></div>
-            <p className="item-link">了解更多→</p>
-          </a>
-          <a href="" className="item" id="item3">
-            <h2 className="item-title">VR互动教室</h2>
-            <div className="item-icon"></div>
-            <p className="item-link">了解更多→</p>
-          </a>
-          <a href="" className="item" id="item4">
-            <h2 className="item-title">VR实训空间</h2>
-            <div className="item-icon"></div>
-            <p className="item-link">了解更多→</p>
-          </a>
-        </section>
-        <HomeFooter />
-      </section>
-    );
-  }
+    render() {
+        var baseUrl = 'http://123.57.53.5/vr/';
+        var iesUrl = baseUrl + 'product/ies/';
+        var kindsUrl = baseUrl + 'product/kinds/';
+        return (
+          <section className="product">
+            <HomeHeader />
+            <section className="product-index">
+              <a href={iesUrl} className="item" id="item1">
+                <h2 className="item-title">IES系统</h2>
+                <div className="item-icon"></div>
+                <p className="item-link">了解更多→</p>
+              </a>
+              <a href={kindsUrl} className="item" id="item2">
+                <h2 className="item-title">VR课堂</h2>
+                <div className="item-icon"></div>
+                <p className="item-link">了解更多→</p>
+              </a>
+              <a href={kindsUrl} className="item" id="item3">
+                <h2 className="item-title">VR互动教室</h2>
+                <div className="item-icon"></div>
+                <p className="item-link">了解更多→</p>
+              </a>
+              <a href={kindsUrl} className="item" id="item4">
+                <h2 className="item-title">VR实训空间</h2>
+                <div className="item-icon"></div>
+                <p className="item-link">了解更多→</p>
+              </a>
+            </section>
+            <HomeFooter />
+          </section>
+        );
+    }
 }
 
 export default Product;
