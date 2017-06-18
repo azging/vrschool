4846d79205ea0bb50716cc5d4344d82c2b6f98b2
zzs <zzspku@qq.com>
2017-06-18 17:58:20 +0800

    上传build

--- src/Product.js
+++ src/Product.js
@@ -8,9 +8,9 @@ class Product extends Component {
     render() {
         var baseUrl = 'http://123.57.53.5/vr/';
         var iesUrl = baseUrl + 'product/ies/';
-        var kindsUrl0 = baseUrl + 'product/kinds/';
-        var kindsUrl1 = baseUrl + 'product/kinds/#1';
-        var kindsUrl2 = baseUrl + 'product/kinds/#2';
+        var kindsUrl1 = baseUrl + 'product/kinds/index.html#1';
+        var kindsUrl2 = baseUrl + 'product/kinds/index.html#2';
+        var kindsUrl3 = baseUrl + 'product/kinds/index.html#3';
         return (
           <section className="product">
             <HomeHeader />
@@ -20,17 +20,17 @@ class Product extends Component {
                 <div className="item-icon"></div>
                 <p className="item-link">了解更多→</p>
               </a>
-              <a href={kindsUrl0} className="item" id="item2">
+              <a href={kindsUrl1} className="item" id="item2">
                 <h2 className="item-title">VR课堂</h2>
                 <div className="item-icon"></div>
                 <p className="item-link">了解更多→</p>
               </a>
-              <a href={kindsUrl1} className="item" id="item3">
+              <a href={kindsUrl2} className="item" id="item3">
                 <h2 className="item-title">VR互动教室</h2>
                 <div className="item-icon"></div>
                 <p className="item-link">了解更多→</p>
               </a>
-              <a href={kindsUrl2} className="item" id="item4">
+              <a href={kindsUrl3} className="item" id="item4">
                 <h2 className="item-title">VR实训空间</h2>
                 <div className="item-icon"></div>
                 <p className="item-link">了解更多→</p>
