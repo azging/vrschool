f577dafdbc97171b67ba908d4153ceadfb0ab045
zzs <zzspku@qq.com>
2017-06-18 18:06:24 +0800

    上传build

--- src/ProductNav.js
+++ src/ProductNav.js
@@ -6,12 +6,24 @@ class ProductNav extends Component {
     hideNav() {
         $("#product-subnav").hide();
     }
+    showKind1() {
+        var baseUrl = 'http://123.57.53.5/vr/';
+        var kindsUrl1 = baseUrl + 'product/kinds/index.html#1';
+        window.location.href=kindsUrl1;
+    }
+    showKind2() {
+        var baseUrl = 'http://123.57.53.5/vr/';
+        var kindsUrl2 = baseUrl + 'product/kinds/index.html#2';
+        window.location.href=kindsUrl2;
+    }
+    showKind3() {
+        var baseUrl = 'http://123.57.53.5/vr/';
+        var kindsUrl3 = baseUrl + 'product/kinds/index.html#3';
+        window.location.href=kindsUrl3;
+    }
     render() {
         var baseUrl = 'http://123.57.53.5/vr/';
         var iesUrl = baseUrl + 'product/ies/';
-        var kindsUrl1 = baseUrl + 'product/kinds/index.html#1';
-        var kindsUrl2 = baseUrl + 'product/kinds/index.html#2';
-        var kindsUrl3 = baseUrl + 'product/kinds/index.html#3';
         return (
           <section className="nav">
             <section id="product-subnav" onMouseLeave={this.hideNav} className="product-subnav">
@@ -20,17 +32,17 @@ class ProductNav extends Component {
                 <img src="http://download.duckr.cn/vrschool/nav/subnav_product_1_icon.png" alt="" className="subnav-icon"/>
                 <p className="subnav-title">IES系统</p>
               </a>
-              <a href={kindsUrl1} className="subnav-item" id="subnav-item2">
+              <a href={this.showKind1} className="subnav-item" id="subnav-item2">
                 <div className="mask"></div>
                 <img src="http://download.duckr.cn/vrschool/nav/subnav_product_2_icon.png" alt="" className="subnav-icon"/>
                 <p className="subnav-title">VR课堂</p>
               </a>
-              <a href={kindsUrl2} className="subnav-item" id="subnav-item3">
+              <a href={this.showkind2} className="subnav-item" id="subnav-item3">
                 <div className="mask"></div>
                 <img src="http://download.duckr.cn/vrschool/nav/subnav_product_3_icon.png" alt="" className="subnav-icon"/>
                 <p className="subnav-title">VR互动教室</p>
               </a>
-              <a href={kindsUrl3} className="subnav-item" id="subnav-item4">
+              <a href={this.showKind3} className="subnav-item" id="subnav-item4">
                 <div className="mask"></div>
                 <img src="http://download.duckr.cn/vrschool/nav/subnav_product_4_icon.png" alt="" className="subnav-icon"/>
                 <p className="subnav-title">VR实训空间</p>
