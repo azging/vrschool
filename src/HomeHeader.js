402305f666390e3a7ffe5e03543f91358bd0b4d7
zzs <zzspku@qq.com>
2017-06-26 00:53:05 +0800

    ceshi

--- src/HomeHeader.js
+++ src/HomeHeader.js
@@ -87,14 +87,14 @@ class HomeHeader extends Component {
                 }
             }
 
-            window.addEventListener("resize", resizeFunc);
+            //window.addEventListener("resize", resizeFunc);
         })();
         var winWidth = $(window).width();
         var right = (winWidth - 1053) - 93 - 148;
         if (right < 10) {
             right = 10;
         }
-        //$(".logo").css("margin-right", right);
+        $(".logo").css("margin-right", right);
         if (this.props.data >=0 && this.props.data <= 5) {
             this.jumpTarget(this.props.data);
         }
@@ -106,7 +106,6 @@ class HomeHeader extends Component {
             <header>
                 <nav className="hero-nav mynav">
                     <img src="http://download.duckr.cn/vrschool/home/topLogo.png" alt="" className="logo" />
-                    <div className="nav-right-box">
                         <div onMouseEnter={this.mouseEnter.bind(this, 0)} onMouseLeave={this.mouseLeave.bind(this, 0)} onClick={this.jumpHome} className="tab">首页</div>
                         <div onMouseEnter={this.mouseEnter.bind(this, 1)} onMouseLeave={this.mouseLeave.bind(this, 1)} onClick={this.jumpProduct} className="tab">产品</div>
                         <div onMouseEnter={this.mouseEnter.bind(this, 2)} onMouseLeave={this.mouseLeave.bind(this, 2)} onClick={this.jumpSolution} className="tab">解决方案</div>
@@ -115,7 +114,6 @@ class HomeHeader extends Component {
                         <div onMouseEnter={this.mouseEnter.bind(this, 5)} onMouseLeave={this.mouseLeave.bind(this, 5)} onClick={this.jumpContact} className="tab" id="contact-us">联系我们</div>
                         <div className="for-school"><a href={loginUrl}>学校登录</a></div>
                         <div className="for-dev"><a href={devUrl}>开发者</a></div>
-                    </div>
                     <span className="target"></span>
                 </nav>
                 <section className="home-product-nav" id="product-nav">
