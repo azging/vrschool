22697cd56e204663ba00f5482ad8d91ff1d4635a
zzs <zzspku@qq.com>
2017-07-02 19:16:20 +0800

    ceshi

--- src/Solution.js
+++ src/Solution.js
@@ -36,22 +36,54 @@ class Solution extends Component {
         return (
             <section className="home">
                 <HomeHeader data="2" />
-                <img className="news-top-slider-bg" src="http://download.duckr.cn/vrschool/news/Slider1.png" />
-                <section className="news-middle-box">
-                    <section className="news-middle-content-box">
-                        <PicText data="http://download.duckr.cn/vrschool/news/banner1.jpg" title="全球首个VR软件视保技术发布-微视酷致力打造健康VR教育" type="2" href="http://47.93.28.215/vr/details/details1/" />
-                        <PicText data="http://download.duckr.cn/vrschool/news/banner2.jpg" title="Vive生态圈大会在深举行，VR教育再成热点！" type="2" href="http://47.93.28.215/vr/details/details2/" />
+                <section className="solution-container-box">
+                    <img id="solution-bg" src="http://download.duckr.cn/vrschool/solution/Bg_1_1.jpg" /> 
+                    <section className="solution-control-opacity">
                     </section>
-                </section>
-                <section className="news-footer-box1">
-                        <PicText data="http://download.duckr.cn/vrschool/news/banner3.png" title="济南VR智慧课堂教育研讨会大获成功，引发广泛关注" type="3" href="http://47.93.28.215/vr/details/details3/" />
-                        <PicText data="http://download.duckr.cn/vrschool/news/banner4.jpg" title="江苏新沂市教育局召开VR智慧课堂研讨会" type="3" href="http://47.93.28.215/vr/details/details4/" />
-                        <PicText data="http://download.duckr.cn/vrschool/news/banner5.jpg" title="微视酷落地常熟探讨VR教学" type="3" href="http://47.93.28.215/vr/details/details5/" />
-                </section>
-                <section className="news-footer-box2">
-                        <PicText data="http://download.duckr.cn/vrschool/news/banner6.png" title="微视酷VR课堂亮相北京小学英语信息化课堂教学研讨会" type="3" href="http://47.93.28.215/vr/details/details6/" />
-                        <PicText data="http://download.duckr.cn/vrschool/news/banner7.jpg" title="VR课堂朝阳润丰小学，探索未来教育新模式" type="3" href="http://47.93.28.215/vr/details/details7/" />
-                        <PicText data="http://download.duckr.cn/vrschool/news/banner8.jpg" title="微视酷VR课堂再现哈市 聚首阳明小学探讨未来教育" type="3" href="http://47.93.28.215/vr/details/details8/" />
+                    <section id="img-text0" className="solution-text1-box">
+                        <p>VR教学教室</p>
+                    </section>
+                    <section id="img-text1" className="solution-text2-box">
+                        <p className="solution-text-title">VR创客教室</p>
+                        <p className="solution-text-content1">STEAM创客教育意在提高学生在科学、技术、工程、数学和艺术领域的能力，结合VR技术，创造沉浸式体验，培养学生的认知体验，激发学习兴趣</p>
+                        <p className="solution-text-content2">应用领域：创意创想；探寻科学原理等</p>
+                    </section>
+                    <section id="img-text2" className="solution-text3-box">
+                        <p className="solution-text-title">VR科学实验室</p>
+                        <p className="solution-text-content1">该方案提供科学实验室的VR形式实验</p>
+                        <p className="solution-text-content2">应用领域：物理实验，化学实验，生物实验等</p>
+                    </section>
+                    <section id="img-text3" className="solution-text4-box">
+                        <p className="solution-text-title">VR研学游学</p>
+                        <p className="solution-text-content1">借助计算机图像技术以三维建模的形式构建虚拟大空间，并且提供配套的导揽功能。完成大空间的游览展示</p>
+                        <p className="solution-text-content2">应用领域：场馆展示；大空间展示；历史遗迹复原等</p>
+                    </section>
+                    <section id="img-text4" className="solution-text5-box">
+                        <p className="solution-text-white-title">VR博物馆</p>
+                        <p className="solution-text-white-content1">可以还原真实的博物馆和其中的文物，还可以突破博物馆传统的陈列方式，更多视角距离的提供文物的展示。</p>
+                        <p className="solution-text-white-content2">应用领域：古生物还原；文物还原；艺术品展示等。</p>
+                    </section>
+                    <section id="img-text5" className="solution-text6-box">
+                        <p className="solution-text-title">VR安全访问站</p>
+                        <p className="solution-text-content1">安全教育通常通过讲解和模拟的方式进行，VR技术创造的沉浸感，可以很大程度上强化安防教育的效果，令培训效果刻骨铭心</p>
+                        <p className="solution-text-content2">应用领域：消防安全；交通安全；设备或工具的安全使用等</p>
+                    </section>
+                    <section id="img-text6" className="solution-text7-box">
+                        <p className="solution-text-white-title">VR实操车间</p>
+                        <p className="solution-text-white-content1">此方案用于解决特定行业教学内容抽象，形式枯燥，实训设配昂贵或损耗严重，实操危险问题</p>
+                        <p className="solution-text-white-content2">应用领域：器械的拆散和组装；器械的结构原理展示等</p>
+                    </section>
+                    <section id="img-text7" className="solution-text8-box">
+                        <p className="solution-text-white-title">VR天下</p>
+                        <p className="solution-text-white-content1">采用专业的航拍，全景拍摄技术的拍摄风景，地貌等影像</p>
+                        <p className="solution-text-white-content2">应用领域：展览展示，风景地貌展示等</p>
+                    </section>
+                    <section id="img-text8" className="solution-text9-box">
+                        <p className="solution-text-white-title">VR安全访问站</p>
+                        <p className="solution-text-white-content1">安全教育通常通过讲解和模拟的方式进行，VR技术创造的沉浸感，可以很大程度上强化安防教育的效果，令培训效果刻骨铭心</p>
+                        <p className="solution-text-white-content2">应用领域：消防安全；交通安全；设备或工具的安全使用等</p>
+                    </section>
+                    
                 </section>
                 <HomeFooter />
             </section>
